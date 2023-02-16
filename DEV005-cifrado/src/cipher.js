d5fdf2b9c2641e46c57a57aab82010d705fadc62
Brina <brixiga1892@gmail.com>
2023-02-16 13:41:30 -0500

    mi primer commit

--- DEV005-cifrado/src/cipher.js
+++ DEV005-cifrado/src/cipher.js
@@ -1,5 +1,27 @@
-const cipher = {
+//const cipher = {
   // ...
-};
+//};
 
-export default cipher;
+function CifrarMsj() {
+  var palabra = document.getElementById("textooriginal").value;
+  var desplazamiento = document.getElementById("espaciado").value;
+  var respuesta = ""
+  desplazamiento = (desplazamiento%26 + 26) % 26;
+  for ( var i = 0; i < palabra.length ; i++) {
+    var ascii = palabra[i].charCodeAt();
+    if ((ascii >= 65 && ascii <= 90)) {
+      respuesta += String.fromCharCode((ascii - 'A'.charCodeAt(0) + desplazamiento)%26 
+        + 'A'.charCodeAt(0)) ;
+    } else if(ascii >= 97 && ascii <= 122){
+        respuesta += String.fromCharCode((ascii-'a'.charCodeAt(0) + desplazamiento) % 26 
+       + 'a'.charCodeAt(0));
+    } else {
+        respuesta += palabra[i]
+    }
+ }
+ alert(respuesta);
+ return respuesta;
+}
+
+//export default cipher;
+
